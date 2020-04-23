d357f80aa9b38a19a07c4ef799d4eef925d9d6a8
Matteo Pierro <pierromatteo@gmail.com>
2020-04-23 16:40:35 +0200

    refactor

--- vendingMachine.js
+++ vendingMachine.js
@@ -1,4 +1,4 @@
-const INSERT_COIN_STATE = "INSERT COIN"
+const INSERT_COIN_STATE = "INSERT COIN";
 const NICKEL_WEIGHT = 5.0;
 const NICKEL_AMOUNT = 0.05;
 const DIME_AMOUNT = 0.10;
@@ -7,8 +7,12 @@ module.exports = function createVendingMachine() {
   let currentAmount = 0;
   return {
     receiveCoin: (weight) => {
-      currentAmount += weight === NICKEL_WEIGHT ? NICKEL_AMOUNT : DIME_AMOUNT;
+      currentAmount += amountForWeight(weight);
     },
     output: () => currentAmount ? `\$${currentAmount.toFixed(2)}` : INSERT_COIN_STATE
   }
+}
+
+function amountForWeight(weight) {
+  return weight === NICKEL_WEIGHT ? NICKEL_AMOUNT : DIME_AMOUNT;
 }
