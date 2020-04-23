0dffd3b9375a892aef2c4eb42d6d80666eef216e
Matteo Pierro <pierromatteo@gmail.com>
2020-04-23 16:34:14 +0200

    use constants instead of magic numbers

--- vendingMachine.js
+++ vendingMachine.js
@@ -1,10 +1,13 @@
 const INSERT_COIN_STATE = "INSERT COIN"
+const NICKEL_WEIGHT = 5.0;
+const NICKEL_AMOUNT = 0.05;
+const DIME_AMOUNT = 0.10;
 
 module.exports = function createVendingMachine() {
   let currentAmount;
   return {
     receiveCoin: (weight) => {
-      currentAmount = weight === 5.0 ? 0.05 : 0.10;
+      currentAmount = weight === NICKEL_WEIGHT ? NICKEL_AMOUNT : DIME_AMOUNT;
     },
     output: () => currentAmount ? `\$${currentAmount.toFixed(2)}` : INSERT_COIN_STATE
   }
