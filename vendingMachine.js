095603336a637e526e7042fb1c740b0547e3b169
Matteo Pierro <pierromatteo@gmail.com>
2020-04-23 15:56:31 +0200

    refactor vendingMachine

---
+++ vendingMachine.js
@@ -0,0 +1,5 @@
+module.exports = function createVendingMachine() {
+  return {
+    output: () => "INSERT COIN"
+  }
+}
--- vendingMachine.test.js
+++ vendingMachine.test.js
@@ -1,10 +1,6 @@
+const createVendingMachine = require('./vendingMachine');
+
 it('should pass', function () {
     const vendingMachine = createVendingMachine();
     expect(vendingMachine.output()).toBe("INSERT COIN");
 });
-
-function createVendingMachine() {
-    return {
-        output: () => "INSERT COIN"
-    }
-}
