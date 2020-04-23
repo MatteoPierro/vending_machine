81afac7588328219c3a749538bd703983b0ecae7
Matteo Pierro <pierromatteo@gmail.com>
2020-04-23 16:31:39 +0200

    make currentAmount a number

--- vendingMachine.js
+++ vendingMachine.js
@@ -4,8 +4,8 @@ module.exports = function createVendingMachine() {
   let currentAmount;
   return {
     receiveCoin: (weight) => {
-      currentAmount = weight === 5.0 ? "0.05" : "0.10";
+      currentAmount = weight === 5.0 ? 0.05 : 0.10;
     },
-    output: () => currentAmount ? `\$${currentAmount}` : INSERT_COIN_STATE
+    output: () => currentAmount ? `\$${currentAmount.toFixed(2)}` : INSERT_COIN_STATE
   }
 }
