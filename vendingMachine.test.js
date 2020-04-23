0f472d85899282fd4a556b583b66219aefd80ce1
Matteo Pierro <pierromatteo@gmail.com>
2020-04-23 16:22:22 +0200

    instantiate vending machine in a beforeEach

--- vendingMachine.test.js
+++ vendingMachine.test.js
@@ -2,13 +2,18 @@ const createVendingMachine = require('./vendingMachine');
 
 const NICKEL_WEIGHT = 5.0;
 
+let vendingMachine;
+
+beforeEach(() => {
+    vendingMachine = createVendingMachine();
+});
+
 it('display insert coin when no coins are inserted', function () {
-    const vendingMachine = createVendingMachine();
     expect(vendingMachine.output()).toBe("INSERT COIN");
 });
 
 it('displays the amount for a nickel', function () {
-    const vendingMachine = createVendingMachine();
     vendingMachine.receiveCoin(NICKEL_WEIGHT);
+
     expect(vendingMachine.output()).toBe("$0.05");
 });
