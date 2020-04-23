4747b5356d9a5afe4dabe6f6c520b8d82edf019c
Matteo Pierro <pierromatteo@gmail.com>
2020-04-23 16:20:49 +0200

    extract constant for nickel weight

--- vendingMachine.test.js
+++ vendingMachine.test.js
@@ -1,5 +1,7 @@
 const createVendingMachine = require('./vendingMachine');
 
+const NICKEL_WEIGHT = 5.0;
+
 it('display insert coin when no coins are inserted', function () {
     const vendingMachine = createVendingMachine();
     expect(vendingMachine.output()).toBe("INSERT COIN");
@@ -7,6 +9,6 @@ it('display insert coin when no coins are inserted', function () {
 
 it('displays the amount for a nickel', function () {
     const vendingMachine = createVendingMachine();
-    vendingMachine.receiveCoin(5.0);
+    vendingMachine.receiveCoin(NICKEL_WEIGHT);
     expect(vendingMachine.output()).toBe("$0.05");
 });
